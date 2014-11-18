cfe78779320f29de45da9de31627119d0387097d
Ngô Thành Long <thanhthi.hn93@gmail.com>
2014-11-18 12:13:39 +0700

    create ping all

---
+++ ping-com3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#@: VDCIT@OpenStack
+#@: Test ip cho cac may
+#@ Compute3
+###############################
+
+echo -e "\e[92m Ping toi dai 10.30.0.0  \e[0m"
+for i in {1,11,21,31,32,33,101,111}; do timeout 1 ping -c 1 10.30.0.$i >/dev/null && echo $_; done
+
+echo -e "\e[92m Ping toi dai 10.40.0.0  \e[0m"
+for i in {1,21,31,32,33}; do timeout 1 ping -c 1 10.20.0.$i >/dev/null && echo $_; done
+
+ 
---
+++ ping-network.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#@: VDCIT@OpenStack
+#@: Test ip cho cac may
+#@ Network
+###############################
+
+echo -e "\e[92m Ping toi dai 10.30.0.0  \e[0m"
+for i in {1,11,21,31,32,33,101,111}; do timeout 1 ping -c 1 10.30.0.$i >/dev/null && echo $_; done
+
+echo -e "\e[92m Ping toi dai 10.40.0.0  \e[0m"
+for i in {1,21,31,32,33}; do timeout 1 ping -c 1 10.40.0.$i >/dev/null && echo $_; done
+
+
+ 
