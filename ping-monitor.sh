282b24b55ce75b3a28554a55b3685c55bc5a2273
Ngô Thành Long <thanhthi.hn93@gmail.com>
2014-11-18 13:57:01 +0700

    update-monitor

---
+++ ping-monitor.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#@: VDCIT@OpenStack
+#@: Test ip cho cac may
+#@ Monitor
+###############################
+
+cho -e "\e[92m Ping toi dai 10.30.0.0  \e[0m"
+for i in {1,11,21,31,32,33,101,111}; do timeout 1 ping -c 1 10.30.0.$i >/dev/null && echo $_; done
